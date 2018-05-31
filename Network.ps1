ae240a1ae97933dd54a603fc48192b8cb2e6c867
fuegans4213 <adr.couturier@gmail.com>
2018-05-31 17:31:13 +0200

    UPDATE Network.ps1
    Première version affiche résultat du ping + DNS

--- Network.ps1
+++ Network.ps1
@@ -1,10 +1,11 @@
 # =======================================================
 
 # NAME: Network.ps1
-# AUTHOR: COUTURIER Adrien
+# AUTHOR: XXXXXXXXXXXXX
 # DATE: 28/05/2018
 #
 # Scan network
+# Verison de PowerShell a utiliser
 #
 # =======================================================
 <#
@@ -18,7 +19,9 @@
             .\Network.ps1 -DebutPlageIP 192.168.178.0 -FinPlageIP 192.168.178.20
 #>
 
-[CmdletBinding(DefaultParameterSetName='CIDR')]
+
+#paramètre a appeler en ligne de commande 
+[CmdletBinding(DefaultParameterSetName='AdresseIP')]
 Param([Parameter(
             Position=0,
             Mandatory=$true,
@@ -30,10 +33,9 @@ Param([Parameter(
                 Mandatory=$true,
                 HelpMessage='Fin de plage IP exemple : 192.168.51.50')]
         [IPAddress]$FinPlageIP)
+        
 
-
-
-    #Fonction permettant de convertir une IPv4 vers le format Int64 
+    #Fonction permettant de convertir une IPv4 vers le format Int64 et vise versa
     function Convert-AdresseIP
     {
           <#
@@ -62,11 +64,13 @@ Param([Parameter(
                 
                 [Parameter(
                     ParameterSetName='Int64',
-                    Position=1, #Permet de ne pas appeler -Int64 
+                    Position=1, #Permet de ne pas appeler -Int64 après le paramètre -AdresseIP
                     Mandatory=$true,
                     HelpMessage='Veuillez entrer une adresse IPV4 sous forme de int64 exemple 2887141128')]
                 [long]$Int64
              ) 
+
+
           switch($PSCmdlet.ParameterSetName)
             {   
             
@@ -81,68 +85,84 @@ Param([Parameter(
                         }
              }
                    
-         $Result = [pscustomobject] 
+             [pscustomobject] 
                   @{   
                         AdresseIP = $AdresseIP
                         Int64 = $Int64
                    }
            
-           return $Result
-           
       }
 
-    #COnvertion des IP en Int64 
+    # Convertion des IPs en Int64 afin d'éffectué une boucle sur la plage d'adresse IP 
     $DebutPlageIP_Int64 = (Convert-AdresseIP -AdresseIP $DebutPlageIP.ToString()).Int64
-    [long]$FinPlageIP_Int64 = (Convert-AdresseIP -AdresseIP $FinPlageIP.ToString()).Int64
+    $FinPlageIP_Int64 = (Convert-AdresseIP -AdresseIP $FinPlageIP.ToString()).Int64
 
+    
+    # On vérifie l'intégrité de la page d'adresse IP renseignée
+    if($DebutPlageIP_Int64 -gt $FinPlageIP_Int64)
+    {
+        [System.Windows.MessageBox]::Show("La plage d'adresse IP renseignée n'est pas bonne ! ","Plage d''adresse IP non valide","Ok","Error")
+        exit        
+    }
 
-$DebutPlageIP_Int64
+    # Calcul du nombre d'adresse à scanner 
+    $NrbAdresseIP = ($FinPlageIP_Int64 - $DebutPlageIP_Int64)
 
-        # ...
+    
+
+        # Pour chaque adresse IP on test PING + DNS + MAC 
         for ($i = $DebutPlageIP_Int64; $i -le $FinPlageIP_Int64; $i++) 
         { 
-            # Convert IP back from Int64
-            $IPv4Address = (Convert-AdresseIP -Int64 $i).IPv4Address                
-
-            $IPv4Address
-<#
-
-    		# Create hashtable to pass parameters
-    		$ScriptParams = @{
-    			IPv4Address = $IPv4Address
-    			Tries = $Tries
-    			DisableDNSResolving = $DisableDNSResolving
-    			EnableMACResolving = $EnableMACResolving
-    			ExtendedInformations = $ExtendedInformations
-                IncludeInactive = $IncludeInactive
-              #>
-    } 
-
-  
-<# #Résolution d'un nom DNS depuis une IP
-   #[System.Net.Dns]::GetHostEntry("172.22.71.8").Hostname
-   
-   
-   
-   
-   
-   
-$StartIPv4Address = "172.22.71.8"
-   [IPaddress]$EndIPv4Address = "172.22.71.15"
-   
-    $StartIPv4Address_Int64 = (Convert-AdresseIP $StartIPv4Address)
-    $EndIPv4Address_Int64 = (Convert-AdresseIP -AdresseIP $EndIPv4Address.ToString())
-
-    $StartIPv4Address_Int64
-#>
+            # On reconvertie le Int64 en IPAddress
+            $AdresseIP = (Convert-AdresseIP -Int64 $i).AdresseIP                
 
 
 
+            # On fait 3 PING 
+            for ($y = 0; $y -lt 3; $y++)
+            {
+                try
+                {
 
+				    $Ping = New-Object System.Net.NetworkInformation.Ping
+				
+				    $Timeout = 1000
 
+				    $PingResult = $Ping.Send($AdresseIP, $Timeout)
 
+				    if($PingResult.Status -eq "Success")
+				    {
+					    $Status = "Up"
+					    break # Si la machine répond sortie de boucle
+				    }
+				    else
+				    {
+					    $Status = "Down"
+				    }
+			    }
+			    catch
+			    {
+				    $Status = "Down"
+				    break # Si erreur sortie de boucle
+                }
 
+            }
 
+            if($Status -eq "Up")
+            {   	
+                    try
+                    {
+                        #Résolution d'un nom DNS depuis une IP
+                        $HostName = [System.Net.Dns]::GetHostEntry($AdresseIP).Hostname
+                    }
+                    catch
+                    {
+                        $HostName = "### No DNS ###"
+                    }
+            }
+            
 
-
-
+            $HostName
+            $Status
+        
+        }
